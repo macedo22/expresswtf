0665b702f48dd58e62ea12ab1430f24641cae817
Alexandra Macedo <macedo22@ucla.edu>
2017-06-12 23:53:49 -0700

    add function prototypes

--- functions.js
+++ functions.js
@@ -5,3 +5,124 @@
  */
 
 
+exports.myDateTime = function () {
+    return Date();
+};
+
+
+
+//self-made functions to export
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end('Hello World!');
+}).listen(8080);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+// Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const clientId = 'UCn9wNpGoW4R8I-skfKghw';
+const clientSecret = 'sU7Db2PGwyTiFk338fP1YX7CSlpTyp8NZ7ap3wvOILXLV8yK3KDN5tR3oO6U9pwx';
+
+const searchRequest = {
+  term:'Four Barrel Coffee',
+  location: 'san francisco, ca'
+};
+
+yelp.accessToken(clientId, clientSecret).then(response => {
+  const client = yelp.client(response.jsonBody.access_token);
+
+  client.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody.businesses[0];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+  });
+}).catch(e => {
+  console.log(e);
+});
+
+
+
+
+/******************Fuctions to create*************/
+
+
+function getRestaurants(zipcode, categories){
+    //do get request based on search criteria
+    //populate an array with first 10 restauarants with for loop
+    //return the array of restaurant matches
+};
+
+
+
+//we should check to see if we can submit a get request specifying at least 3 stars
+function isDecent(/*pass restarant id or whole json object*/){
+    //parse out # of stars
+    //return whether or not it is at least 3 stars (bool)
+};//
+
+
+
+
+
+function isArray(myArray) {
+    return myArray.constructor.toString().indexOf("Array") > -1;
+};
+
+
+
+
+
+//Function to store first 10 results into an array
+function getTopResults(resArray){
+    //send get request for restauarants that fit criteria
+    //populate array with restaurant matches
+};
+
+// Parses out and checks if restauarant has category
+//this may be unenecessary if yelp let's us screen for criteria upfront
+function hasCriterion(category){
+    //return bool
+};
+
+
+
+/*The following function is very different if we can specify to yelp to only
+ * return restauarants that fit the criteria in the first place... which is 
+ * probably what it does lol*/
+
+//takes zipcode and criteria and does get request
+//var zipcode ="";
+//var criterion=["","","","","",""];
+function getRestaurantById(zipcode, criterion){
+    if(isArray(criterion)&&criterion.length!==0){  //check to make sure criterion is an array
+        const numOfResults=10;
+        var results=[];
+        var matchedResults=[];
+        //get results and store in results array (only 10 for now)
+        for(var i=0;i<results.length;i++){ // check all the results from yelp
+            var match=false;
+            for(var j=0;criterion[j]!=="";j++){
+                //if criterion not found, exit loop; if it is, just continue
+                //i.e.: if(results[i].hasCriterion(criterion[j])===false)
+                //            exit;
+            }
+            if(status===true){
+                //add result to mtachedResults
+            }
+        }
+    }
+};
--- index.js
+++ index.js
@@ -1,38 +1,67 @@
-const express = require('express')
-const app = express()
+
+//include function.js?
+
+const express = require('express');
+const app = express();
+var http = require('http');
+var dt = require('./functions');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end('Hello World!');
+}).listen(8080);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-'use strict';
-
-const yelp = require('yelp-fusion');
-
-// Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
-// from https://www.yelp.com/developers/v3/manage_app
-const clientId = 'UCn9wNpGoW4R8I-skfKghw';
-const clientSecret = 'sU7Db2PGwyTiFk338fP1YX7CSlpTyp8NZ7ap3wvOILXLV8yK3KDN5tR3oO6U9pwx';
-
-const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
-};
-
-yelp.accessToken(clientId, clientSecret).then(response => {
-  const client = yelp.client(response.jsonBody.access_token);
-
-  client.search(searchRequest).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
-    const prettyJson = JSON.stringify(firstResult, null, 4);
-    console.log(prettyJson);
-  });
-}).catch(e => {
-  console.log(e);
+  res.send('Hello World!');
 });
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+
+
+
+
 //document.getElementById("test").innerHTML="Goodbye world!";
+
+
+/*************************************
+ * 
+ * This file should mostly be filled with calling functions in function.js upon an event (i.e. zipcode is entered)?
+ * 
+ * 
+ * 
+ * 
+ * 
+ ******************************************/
+
+
+//event: if entered zipcode--> call function (that gets matching results, randomly picks one, returns it)
+//and use the returned information to put into HTML tags in resultPage
+//pseudocode for this: (NOTE: THIS IS SHITTY AND CAN DEFINITELY BE SIMPLIFIED IT'S SUPA LATE RN)
+/*if user searches (i.e. specifies zip code and categories)     //the event
+ *       var JSONobj = getRestaurants(zipcode, categories[]);  //returns array of restauarant objects?
+ *       generate random number 0-9, inclusive
+ *       select the random index from the array of matched restauarants
+ *       parse out name, phone number, reviews, address, etc.
+ *       insert these elements into resultPage.html to display them to client
+ *       assign false boolean to this restaurant (remove it from list to pick from if user rerandomizes)
+ *      
+ *       if user clicks rerandomize
+ *           var status=false;
+ *           var options=9;
+ *           while (status=false){
+ *              if(options===0){
+ *                  "There are no more options"
+ *                  exit loop and navigate back
+ *              }
+ *              generate a new random number && index 
+ *              if(index's bool ===true){
+ *                  display result
+ *                  options--
+ *              }
+ *          }
+ */
